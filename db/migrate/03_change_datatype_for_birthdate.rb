ed2a08d7bfc5aeb99fb7f33e3495761d7089d259
howsilence <statton0104@gmail.com>
2022-02-27 23:15:14 -0500

    commit tree ae4c4f08432984e8c1ee271246ea7f7f370362ba

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,11 @@
+# ChangeDatatypeForBirthdate < ActiveRecord::Migration[6.1]
+# def change
+#     change_column :students, :birthdate, :datetime
+# end
+# end
+
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[6.1]
+    def change
+      change_column :students, :birthdate, :datetime
+    end
+  end
